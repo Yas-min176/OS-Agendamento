2800707421465d3ee7ea023d9c0855b5d11029ed
Yasmin Sena <102176193+Yas-min176@users.noreply.github.com>
2022-04-10 16:09:44 -0300

    Update abertura_os_main.js

--- abertura_os_main.js
+++ abertura_os_main.js
@@ -44,8 +44,8 @@ function abertura_os () {
 }
 function mensagem(texto)
 {
-    var chat_id = 687820098;
-    var bot_id = "5052096013:AAG-YM58pQZscUpLvMJS-DGwen5l6dC_6kA";
+    var chat_id = ;
+    var bot_id = "";
     var url = "https://api.telegram.org/bot" + bot_id + "/sendMessage?chat_id=-" + chat_id + "&text=" + texto;
     Logger.log(url);
 
