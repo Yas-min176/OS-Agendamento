af3e60182255fb2533e1107ebc0c49208c82c286
Yasmin Sena <102176193+Yas-min176@users.noreply.github.com>
2022-04-10 16:09:05 -0300

    Update Dados_bot.js

--- Dados_bot.js
+++ Dados_bot.js
@@ -1,7 +1,7 @@
 function mensagem()
 {
-    var chat_id = 687820098;
-    var bot_id = "5052096013:AAG-YM58pQZscUpLvMJS-DGwen5l6dC_6kA";
+    var chat_id = "" ;
+    var bot_id = "";
     var texto = "Solicitante: /nEmail: /nSolicitação: /nIrá utilizar: /nData limite: /nEmpresa: /nDestino do projeto: /nTelefone: /nMaterial utilizado no corte: ";
     texto = encodeURICompent(texto);
     var url = "https://api.telegram.org/bot" + bot_id + "/sendMessage?chat_id=-" + chat_id + "&text=" + texto;
